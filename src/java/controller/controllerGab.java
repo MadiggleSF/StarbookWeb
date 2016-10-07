0f65abd0ab96f19b549008005fb505ff41374b88
gabrielledodane <gabrielledodane@gmail.com>
2016-10-07 13:58:11 +0200

    Update

--- src/java/controller/controllerGab.java
+++ src/java/controller/controllerGab.java
@@ -84,11 +84,9 @@ public class controllerGab extends HttpServlet {
                     response.addCookie(cccc);
                 }
             }
-            
-            
+
             // si la personne n'est pas dans la base de données, prévoir un message 
             // " pensez à vous inscrire" + lien "s'inscrire"
-            
         }
 
         // GESTION DU BEAN SIGNUP 
@@ -98,80 +96,84 @@ public class controllerGab extends HttpServlet {
             BeanSignUp bSignUp = (BeanSignUp) session.getAttribute("beanSignUp");
             if (bSignUp == null) {
                 bSignUp = new BeanSignUp();
-                session.setAttribute("beanSignUp",bSignUp);
+                session.setAttribute("beanSignUp", bSignUp);
             }
-            if (request.getParameter("Validation") != null){
-                boolean reussi= true;
+            if (request.getParameter("Validation") != null) {
+                boolean reussi = true;
                 bSignUp.insertSignUp(request.getParameter("surname"),
-                                     request.getParameter("firstname"),
-                                     request.getParameter("password"),
-                                     request.getParameter("mail"),
-                                     request.getParameter("cell"),
-                                     request.getParameter("landline"),
-                                     Date.valueOf(request.getParameter("dob")));
+                        request.getParameter("firstname"),
+                        request.getParameter("password"),
+                        request.getParameter("mail"),
+                        request.getParameter("cell"),
+                        request.getParameter("landline"),
+                        Date.valueOf(request.getParameter("dob")));
+
+            //prévoir si insertion validée -> "inscription validée" et retour
+            //vers la homepage
+            //prévoir si pbs d'insertion, message " Vous n'avez pas rempli correctement les champs"
+//            if (bSignUp.insertSignUp()) == true { 
+//              url = "/WEB-INF/jspSignUpValidation.jsp";  
+//                
+//            }
+//                
+//            if (bSignUp.insertSignUp()) == false {
+//                 
+//                
+//        }
+//        }
+                /*BeanLogin bLogin = (BeanLogin) session.getAttribute("beanLogin");
+                 if (bLogin == null) {
+                 bLogin = new BeanLogin();
+                 session.setAttribute("beanLogin", bLogin);
+                 }
+                 if (bLogin.check(request.getParameter("login"),
+                 request.getParameter("password"))) {*/
+                request.getRequestDispatcher(url).include(request, response);
+            }
+
+            // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+            /**
+             * Handles the HTTP <code>GET</code> method.
+             *
+             * @param request servlet request
+             * @param response servlet response
+             * @throws ServletException if a servlet-specific error occurs
+             * @throws IOException if an I/O error occurs
+             */
+            @Override
+            protected void doGet
+            (HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+                processRequest(request, response);
+            }
+
+            /**
+             * Handles the HTTP <code>POST</code> method.
+             *
+             * @param request servlet request
+             * @param response servlet response
+             * @throws ServletException if a servlet-specific error occurs
+             * @throws IOException if an I/O error occurs
+             */
+            @Override
+            protected void doPost
+            (HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+                processRequest(request, response);
+            }
+
+            /**
+             * Returns a short description of the servlet.
+             *
+             * @return a String containing servlet description
+             */
+            @Override
+            public String getServletInfo
             
-                
-                //prévoir si insertion validée -> "inscription validée" et retour
-                //vers la homepage
-                
-                //prévoir si pbs d'insertion, message " Vous n'avez pas rempli correctement les champs"
-                
-                
-                
-        }
-        }
-               
-        
-         /*BeanLogin bLogin = (BeanLogin) session.getAttribute("beanLogin");
-         if (bLogin == null) {
-         bLogin = new BeanLogin();
-         session.setAttribute("beanLogin", bLogin);
-         }
-         if (bLogin.check(request.getParameter("login"),
-         request.getParameter("password"))) {*/
-
-        request.getRequestDispatcher(url).include(request, response);
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
+                () {
         return "Short description";
-    }// </editor-fold>
+            }// </editor-fold>
 
-
+        }
+    }
 }
-
---
+++ web/WEB-INF/jspSignUpError.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : jspSignUpError
+    Created on : 7 oct. 2016, 13:38:17
+    Author     : cdi307
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
